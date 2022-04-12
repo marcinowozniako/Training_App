9f097e2684975b09ce2c14f972fafcf6c336140e
marcinowozniako <marekboniek@gmail.com>
2022-04-13 01:32:14 +0200

    heroku

---
+++ staticfiles/scripts/form.js
@@ -0,0 +1,9 @@
+if (document.title==='Training Log Workout'){
+    const date = document.getElementById('id_date')
+date.type = 'date'
+}
+
+if (document.title==='Training Log Workout List'){
+    const text = document.querySelector('#text')
+console.log(text)
+}
