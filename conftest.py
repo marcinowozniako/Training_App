0bd83a560eb00cd449178675333f56fe431bac0e
marcinowozniako <marekboniek@gmail.com>
2022-04-20 23:50:12 +0200

    test login, logout, all view for logged user

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from django.apps import apps
+from django.contrib.auth.models import User, Group, Permission
+from django.contrib.contenttypes.models import ContentType
+
+from training.apps import TrainingConfig
+
+
+@pytest.fixture(scope='function')
+def created_user(db):
+    user = User.objects.create(username='tester')
+    user.set_password('ExamplePass')
+    user.save()
+    group_app, created = Group.objects.get_or_create(name=TrainingConfig.name)
+
+    models = apps.all_models[TrainingConfig.name]
+    for model in models:
+        content_type = ContentType.objects.get(
+            app_label=TrainingConfig.name,
+            model=model
+        )
+        permissions = Permission.objects.filter(content_type=content_type)
+        user.groups.add(group_app)
+        group_app.permissions.add(*permissions)
--- tests/test_views.py
+++ tests/test_views.py
@@ -3,6 +3,19 @@ import datetime
 from django.urls import reverse
 
 
+def test_login_user(created_user, client):
+    logged_in = client.login(username='tester', password='ExamplePass')
+
+    assert logged_in
+
+
+def test_logout_user(created_user, client):
+    client.logout()
+    response = client.get('/users/logout')
+
+    assert response.status_code == 302
+
+
 def test_home_page(client):
     url = reverse('home:home')
     response = client.get(url)
@@ -27,58 +40,103 @@ def test_register_page(client):
     assert '<h1>Register</h1>' in response.content.decode('utf-8')
 
 
-def test_add_exercise_page(client):
+def test_add_exercise_page(created_user, client):
     url = reverse('training:create-exercise')
     response = client.get(url)
 
     assert response.status_code == 403
     assert '<h1>You need to be logged to view this site!!</h1>' in response.content.decode('utf-8')
 
+    client.login(username='tester', password='ExamplePass')
+    url = reverse('training:create-exercise')
+    response = client.get(url)
 
-def test_add_training_page(client):
+    assert response.status_code == 200
+
+
+def test_add_training_page(created_user, client):
     url = reverse('training:create-training')
     response = client.get(url)
 
     assert response.status_code == 403
     assert '<h1>You need to be logged to view this site!!</h1>' in response.content.decode('utf-8')
 
+    client.login(username='tester', password='ExamplePass')
+    url = reverse('training:create-training')
+    response = client.get(url)
 
-def test_add_training_plan_page(client):
+    assert response.status_code == 200
+
+
+def test_add_training_plan_page(created_user, client):
     url = reverse('training:create-training-plan-name')
     response = client.get(url)
 
     assert response.status_code == 403
     assert '<h1>You need to be logged to view this site!!</h1>' in response.content.decode('utf-8')
 
+    client.login(username='tester', password='ExamplePass')
 
-def test_create_training_plan_page(client):
+    url = reverse('training:create-training-plan-name')
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+
+def test_create_training_plan_page(created_user, client):
     url = reverse('training:create-training-plan')
     response = client.get(url)
 
     assert response.status_code == 403
     assert '<h1>You need to be logged to view this site!!</h1>' in response.content.decode('utf-8')
 
+    client.login(username='tester', password='ExamplePass')
+    url = reverse('training:create-training-plan')
+    response = client.get(url)
 
-def test_current_training_plan_page(client):
+    assert response.status_code == 200
+
+
+def test_current_training_plan_page(created_user, client):
     url = reverse('training:list')
     response = client.get(url)
 
     assert response.status_code == 403
     assert '<h1>You need to be logged to view this site!!</h1>' in response.content.decode('utf-8')
 
+    client.login(username='tester', password='ExamplePass')
 
-def test_add_workout_page(client):
+    url = reverse('training:list')
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+
+def test_add_workout_page(created_user, client):
     url = reverse('training:workout')
     response = client.get(url)
 
     assert response.status_code == 403
     assert '<h1>You need to be logged to view this site!!</h1>' in response.content.decode('utf-8')
 
+    client.login(username='tester', password='ExamplePass')
+    url = reverse('training:workout')
+    response = client.get(url)
 
-def test_workout_list_page(client):
+    assert response.status_code == 200
+
+
+def test_workout_list_page(created_user, client):
     url = reverse('training:workout-list', kwargs={'year': datetime.datetime.now().isocalendar()[0],
                                                    'week': datetime.datetime.now().isocalendar()[1]})
     response = client.get(url)
 
     assert response.status_code == 403
     assert '<h1>You need to be logged to view this site!!</h1>' in response.content.decode('utf-8')
+
+    client.login(username='tester', password='ExamplePass')
+    url = reverse('training:workout-list', kwargs={'year': datetime.datetime.now().isocalendar()[0],
+                                                   'week': datetime.datetime.now().isocalendar()[1]})
+    response = client.get(url)
+
+    assert response.status_code == 200
